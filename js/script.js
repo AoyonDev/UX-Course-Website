0afd07d5091c464de1a2f1fa27e2873f66aefc3f
AoyonDev <aoyonislam010@gmail.com>
2023-05-19 03:20:34 +0600

    COURSE PRICING PART DONE
    PRICING PART DONE

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $('.pricing-main-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow: '.prev',
+        nextArrow: '.next',
+        infinite: true,
+        // speed: 500,
+        // fade: false,
+        // cssEase: 'linear',
+        centerMode: true,
+        centerPadding: "0px",
+        });
+});

f32169a78554a23eed7e8427e5cc85be8ab9109c
AoyonDev <aoyonislam010@gmail.com>
2023-05-20 15:55:46 +0600

    UPDATED

--- js/script.js
+++ js/script.js
@@ -15,4 +15,21 @@ $(function(){
         centerMode: true,
         centerPadding: "0px",
         });
+
+    $('.review-main-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        // prevArrow: '.prev',
+        // nextArrow: '.next',
+        infinite: true,
+        // speed: 500,
+        // fade: false,
+        // cssEase: 'linear',
+        centerMode: true,
+        centerPadding: "0px",
+        });
+
 });
